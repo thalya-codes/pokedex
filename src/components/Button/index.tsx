e6c4b266710249669cfac10a2e3d3788699d192c
Thalya Stéffany <thalyacoiimbra@gmail.com>
2022-12-16 19:32:15 -0300

    feat: implementa função responsável por manipular a quantidade de botões a serem exibidos de acordo com o tamanho da tela

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { IProps } from '../../interfaces/Button';
 import './style.scss';
 
-const max_buttons = 5;
-const side_buttons_quant = (max_buttons - 1) / 2;
 
 function Button({  limit, offset ,total_page, setOffset }: IProps) {
-
-	const currentPage = offset ? (offset / limit) + 1 : 1;
+	const [maxButtons, setMaxButtons] = useState(5);
+	
 	const pages = Math.ceil(total_page / limit);
+	const currentPage = offset ? (offset / limit) + 1 : 1;
+
+	const side_buttons_quant = (maxButtons - 1) / 2;
 	const button_base = Math.max(currentPage - side_buttons_quant, 0);
+	const mobile_screen_sm = 768;
 	
 	const onChangePage = (page: number):void => {
 		setOffset((page - 1) * limit);
 	};
+
+	const handleMaxButtonsQuantity = (): void => {	
+		const currentScreenWidth = window.innerWidth;
+		
+		if (currentScreenWidth <= mobile_screen_sm) {
+			setMaxButtons(3)
+		} else {
+			setMaxButtons(5);
+		};
+	};
+
+	useEffect(() => {
+		handleMaxButtonsQuantity();
+	});
+	
+	window.addEventListener('resize', (): void => handleMaxButtonsQuantity());	
 
 	return (
 		<div className='buttons-container'>
@@ -28,14 +46,19 @@ function Button({  limit, offset ,total_page, setOffset }: IProps) {
 				>Prev</button>
 			</li>
 			{
-				Array.from({ length: Math.min(max_buttons, pages) })
+				Array.from({ length: Math.min(maxButtons, pages) })
 					.map((_,index) => index + button_base)
 					.map((page: number) => (
 						<li key={page++}>
 							<button
 								className={`
-									buttons-container__button ${page+1 === currentPage ? 'buttons-container__button--active' : ''}`}
+									buttons-container__button
+									${ page+1 === currentPage ? 
+										'buttons-container__button--active' : ''
+									}`}
+
 								onClick={() => onChangePage(page) }
+								
 							>{page+1}</button>
 						</li>
 					))
